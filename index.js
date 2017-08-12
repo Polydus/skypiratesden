dd5361d59e9d341e3dbf69a899f8391eb722a3a2
leonard <contact@polydus.com>
2017-08-12 14:16:18 +0200

    137 crash fix

--- index.js
+++ index.js
@@ -1,12 +1,22 @@
+function getRandomInt(min, max) {
+    var min = Math.ceil(min);
+    var max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
 var express = require('express');
+var gumerPSN = require(__dirname + '/lib/psn.js');
 var app = express();
 
-app.set('port', (process.env.PORT || 5000));
+var zodiacId = 'NPWR11367_00';
+var accounts = [];
 
+app.set('port', (process.env.PORT || 8080));
 app.use(express.static(__dirname + '/public'));
-
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
+
+
 
 app.get('/', function(request, response) {
   response.render('pages/index');
